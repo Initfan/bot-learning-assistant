3ff5bbc2fb6b3201fadad147b64c679309cc214e
Initfan <feeinitfan@gmail.com>
2025-07-12 10:16:01 +0700

    Fix : Throwing error while setting webhook not given url

--- index.js
+++ index.js
@@ -14,18 +14,23 @@ app.post("", (req, res) => {
 	if (update.message && update.message.text) {
 		const chatId = update.message.chat.id;
 		const text = update.message.text;
-
-		if (text === "/start")
-			sendMessage(chatId, "Hello, What language do you want to learn?");
 	}
 
 	res.sendStatus(200);
 });
 
 app.get("/setWebhook", (req, res) => {
-	console.log(res.params);
-	setWebhook(req.params.url);
-	res.send("Webhook info endpoint");
+	if (!req.query.url) {
+		return res.status(400).send("URL parameter is required");
+	}
+	setWebhook(req.query.url)
+		.then(() => {
+			console.log("Webhook set successfully");
+		})
+		.catch((error) => {
+			console.error("Error setting webhook:", error);
+			return res.status(500).send("Error setting webhook");
+		});
 });
 
 app.listen(3000, () => console.log("server running"));
